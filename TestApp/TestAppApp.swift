1dee805bfcc67ce62793e14c7d419d0a746ff4f0
Alyzee Sosa <69488327+alyzees@users.noreply.github.com>
2023-07-14 16:08:10 -0700

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Alyzee Sosa on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
